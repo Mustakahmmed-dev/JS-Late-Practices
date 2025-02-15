f26ccac337276df2fcbf4c0146bd65e5459e4c62
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-15 22:34:27 +0600

    Late practices of Math related operations in JS

---
+++ math.js
@@ -0,0 +1,17 @@
+// abs, round, ceil, floor, random, etc
+
+// abs: Remove the Math operators and shows the gap between
+console.log("Abs: ", Math.abs(5-20));
+
+// round: takes its nearest integer
+console.log("Round : ", Math.round(20.6));
+
+// ceil: this takes to its higher level whatever the decimal number is
+console.log("Ceil :", Math.ceil(5.0001));
+
+// floor: takes its decimal number to the lowest integer
+console.log("Floor: ", Math.floor(32.7));
+
+// random: it creates random numbers
+let randomNum = Math.random();
+console.log("Random: ", randomNum);
