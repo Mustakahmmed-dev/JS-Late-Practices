7acb0e345b35202d0f1804233a82296adad3a2ad
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-19 14:44:10 +0600

    Addition the values of string splits and converting to num

--- problem3.js
+++ problem3.js
@@ -1,14 +1,27 @@
 // split a string value with non-numerics and sum up them using loop, converting strings to num,
 
-let amount = "$512jljdf503";
+let amount = "$512jl jdf5 03";
 // let newValue = amount.split("");
 
 // Looping way 1
 let sum = 0;
 for(let i of amount){   
-  if(!isNaN(i)){
+  if(!isNaN(i) && i !== " "){
     sum += parseInt(i); 
   } 
 }
-// console.log("Way 1: ", sum);
+console.log("Way 1: ", sum);
+
+
+// Way 2
+let amount2 = "$ 8 93 ";
+let amount2Split = amount2.split("");
+let sum2 = 0;
+for(let i = 0; i < amount2Split.length; i++){
+    if(!isNaN(amount2Split[i]) && amount2Split[i] !== " "){
+        sum2 += parseInt(amount2Split[i]);
+    }
+}
+console.log("Way 2:", sum2)
+
 
