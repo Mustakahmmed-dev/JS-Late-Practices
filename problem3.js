13245b60f6ee06bae911cf0c6babc94d4eae5e76
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-20 11:25:46 +0600

    catching out chars in different array

--- problem3.js
+++ problem3.js
@@ -22,6 +22,40 @@ for(let i = 0; i < amount2Split.length; i++){
         sum2 += parseInt(amount2Split[i]);
     }
 }
-console.log("Way 2:", sum2)
+// console.log("Way 2:", sum2)
+
+// let numbers = "#232456";
+// let number = numbers.split("");
+
+// let sum3 = 0;
+// for(let i = 0; i < number.length; i++){
+//   let num = number[i];
+//   if(!isNaN(num)){
+//     sum3 += parseInt(num);
+//   }
+// }
+// console.log(sum3);
+
+
+// Bringing out the len and sum of string inputs
+let numbers2 = "323 53#2 5$ 5rg34";
+
+let emptyTotal = 0;
+let totalNumValue = 0;
+let specialChars = 0;
+for(let num of numbers2){
+  if(num === " "){
+    emptyTotal += num.length;
+  }
+  else if(!isNaN(num)){
+    totalNumValue += parseInt(num);
+  }
+  else{
+    specialChars += num.length;
+  }
+}
+console.log("Empty Spaces: ", emptyTotal);
+console.log("Total Sum: ", totalNumValue);
+console.log("Total Special Chars: ", specialChars);
 
 
