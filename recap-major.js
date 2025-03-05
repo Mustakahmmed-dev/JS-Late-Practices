ff52adbd3cc07f8e452659556e24c0bb6b453b54
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-03-05 14:39:02 +0600

    Default values setup for additions and multiplications in an accurative way

--- recap-major.js
+++ recap-major.js
@@ -9,4 +9,16 @@ callMyName("Mustak Ahmmed");
 // Arrow function
 const myName = (name) => `Welcome back ${name}`;
 
-console.log(myName("Mustak"));
+// console.log(myName("Mustak"));
+
+// Function default values setup
+function sum(a, b=0){
+    return a + b;
+}
+const result = sum(25);
+console.log("Default value setup for addition", result);
+
+// default value setup for multiplication
+const multiply = (a, b=1) => a * b ;
+
+console.log(multiply(10, 20));
