61d9f11d52cebfb0cea2672127f65480a0ec2d37
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-03-05 14:33:43 +0600

    Regular and Arrow functions

---
+++ recap-major.js
@@ -0,0 +1,12 @@
+// Two types of Functions: Regular Function & Arrow function
+
+// Regular function
+function callMyName(name){
+    // console.log(`Hi ${name}`);
+}
+callMyName("Mustak Ahmmed");
+
+// Arrow function
+const myName = (name) => `Welcome back ${name}`;
+
+console.log(myName("Mustak"));
